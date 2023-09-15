0ce7b67e5063eaeeb4083f03d654490997936a95
exteriorduke319 <yarnevictanner@gmail.com>
2023-09-15 09:59:15 -0400

    Initial

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scan = new Scanner(System.in);
+        int number;
+
+        System.out.println("Enter a whole number: ");
+        number = scan.nextInt();
+        if (number % 2 == 0) {
+            System.out.println("Your number is even");
+        } else {
+            System.out.println("Your number is odd");
+        }
     }
 }
